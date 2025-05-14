a30fe4477f8fcedf8faaecede8ca7cdec132d295
阿莹 <aying@staruniongame.com>
2025-05-14 21:52:14 +0800

    feat: martini cors middleware

--- core/martini.go
+++ core/martini.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/innsanes/serv"
+	"net/http"
 	"strconv"
 )
 
@@ -42,7 +43,27 @@ func (s *Martini) BeforeServe() (err error) {
 func (s *Martini) Serve() (err error) {
 	gin.SetMode(gin.ReleaseMode)
 	s.M = martini.New(s.bfs...)
-	s.Engine.Use(s.Logger(), gin.Recovery())
+	s.Engine.Use(s.Logger(), gin.Recovery(), func(context *gin.Context) {
+		method := context.Request.Method
+		// 1. [必须]接受指定域的请求，可以使用*不加以限制，但不安全
+		//context.Header("Access-Control-Allow-Origin", "*")
+		context.Header("Access-Control-Allow-Origin", context.GetHeader("Origin"))
+		fmt.Println(context.GetHeader("Origin"))
+		// 2. [必须]设置服务器支持的所有跨域请求的方法
+		context.Header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+		// 3. [可选]服务器支持的所有头信息字段，不限于浏览器在"预检"中请求的字段
+		context.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, X-Token")
+		// 4. [可选]设置XMLHttpRequest的响应对象能拿到的额外字段
+		context.Header("Access-Control-Expose-Headers", "Access-Control-Allow-Headers, X-Token")
+		// 5. [可选]是否允许后续请求携带认证信息Cookir，该值只能是true，不需要则不设置
+		context.Header("Access-Control-Allow-Credentials", "true")
+		// 6. 放行所有OPTIONS方法
+		if method == "OPTIONS" {
+			context.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		context.Next()
+	})
 
 	// Register router
 	routerGroup := s.Engine.Group("/api")
--- go.mod
+++ go.mod
@@ -8,6 +8,9 @@ require (
 	github.com/innsanes/serv v0.0.0-20240810060752-f3b5b43e32e5
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.27.0
+	golang.org/x/crypto v0.26.0
+	gorm.io/driver/mysql v1.5.7
+	gorm.io/gorm v1.25.11
 )
 
 require (
@@ -38,12 +41,9 @@ require (
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.9.0 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/driver/mysql v1.5.7 // indirect
-	gorm.io/gorm v1.25.11 // indirect
 )
